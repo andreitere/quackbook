c38a125c7439c9c175de43eac6d5fbaca805733f
Andrei Terecoasa <terecoasa.andrei@gmail.com>
2024-11-09 19:40:51 +0200

    f

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import {defineConfig} from "vite";
 import path from "node:path";
 import vue from "@vitejs/plugin-vue";
 import tailwind from "tailwindcss";
-import typography from "@tailwindcss/typography";
 import autoprefixer from "autoprefixer";
 import Unfonts from "unplugin-fonts/vite";
 import Icons from "unplugin-icons/vite";
