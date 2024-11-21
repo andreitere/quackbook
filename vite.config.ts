5b8c4d5202c31c1a6436a4c262a12c1edf10036c
Andrei Terecoasa <terecoasa.andrei@gmail.com>
2024-11-21 16:39:51 +0200

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -14,9 +14,6 @@ export default defineConfig(({mode}) => {
   return {
     build: {
       sourcemap: true,
-      rollupOptions: {
-        external: ["react-async-hook"]
-      }
     },
     css: {
       postcss: {
